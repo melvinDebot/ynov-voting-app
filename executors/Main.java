74cdee251e1b73b83bc3e5b729ae32fdecc1c687
Jean-Pascal MEWENEMESSE <scalpacademy.io@gmail.com>
2023-03-24 13:13:07 +0100

    correction best flight

---
+++ executors/Main.java
@@ -0,0 +1,7 @@
+package executors;
+
+public class Main {
+    public static void main(String[] args) {
+        CompletableFutureDemo.show();
+    }
+}
--- lamba/Main.java
+++ lamba/Main.java
@@ -6,16 +6,6 @@ import java.util.function.Supplier;
 public class Main {
 
     public static void main(String[] args) {
-        var future = CompletableFuture.supplyAsync(()->1);
-        future.thenAccept((result)-> {
-            System.out.println(Thread.currentThread().getName());
-            System.out.println(result);
-        });
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
